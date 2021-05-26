8b640eae35e1980a0bc06bdd544fcd8ce17e1fae
yikezawa <y.ikezawa93@gmail.com>
2021-05-26 23:42:31 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -10,5 +10,17 @@ using namespace std;
 static const int MAX = 2000000;
 
 int main(){
+    int R[MAX], n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> R[i];
 
+    int maxv = -2000000;
+    int minv = R[0];
+
+    for (int i = 1; i < n; i++) {
+        maxv = max(maxv, R[i] - minv);
+        minv = min(minv, R[i]);
+    }
+
+    cout << maxv << endl;
 }
