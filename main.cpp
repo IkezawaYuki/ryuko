9c00dda5a9b68298018a720973e91a3f2de0ae47
yikezawa <y.ikezawa93@gmail.com>
2021-05-28 23:58:56 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -7,37 +7,54 @@
 
 using namespace std;
 
-void trace(int A[], int N) {
+void trace(int R[], int N) {
     for (int i = 0; i < N; i++) {
-        if (i > 0){
+        if (i > 0) {
             cout << " ";
         }
-        cout << A[i];
+        cout << R[i];
     }
     cout << endl;
 }
 
-void insertionSort(int A[], int N) {
+void insertionSort(int R[], int N) {
     for (int i = 1; i < N; i++) {
-        int v = A[i];
         int j = i - 1;
-        while (j >= 0 && v < A[j]) {
-            A[j+1] = A[j];
+        int v = R[i];
+        while (j >= 0 && v < R[j]) {
+            R[j+1] = R[j];
             j--;
         }
-        A[j+1] = v;
-        trace(A, N);
+        R[j+1] = v;
+        trace(R, N);
     }
 }
 
-int main(){
-    int A[100], N;
-
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
+int bubbleSort(int R[], int N) {
+    int sw = 0;
+    bool flag = 1;
+    for (int i = 0; flag; i++) {
+        flag = 0;
+        for (int j = N - 1; j >= i + 1; j--) {
+            if (R[j] < R[j-1]){
+                swap(R[j], R[j-1]);
+                sw++;
+                flag = 1;
+            }
+        }
     }
+    return sw;
+}
 
-    trace(A, N);
-    insertionSort(A, N);
+int main(){
+    int R[100], N;
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> R[i];
+
+    trace(R, N);
+//    insertionSort(R, N);
+
+    int sw = bubbleSort(R, N);
+    cout << sw << endl;
+    trace(R, N);
 }
