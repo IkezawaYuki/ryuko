cad92d77b2bebc86df50ebb52e07bd478fe8b78b
yikezawa <y.ikezawa93@gmail.com>
2021-05-30 15:13:42 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -7,75 +7,43 @@
 
 using namespace std;
 
-struct Card {
-    char suit;
-    int value;
-};
+int cnt;
+vector<int>G;
 
-void bubble(Card C[], int N) {
-    for (int i = 0; i < N; i++) {
-        for (int j = N - 1; i <= j; j--) {
-            if (C[j-1].value > C[j].value) {
-                Card t = C[j];
-                C[j] = C[j-1];
-                C[j-1] = t;
-            }
+void insertionSort(int A[], int N, int gap) {
+    for (int i = gap; i < N; i++) {
+        int v = A[i];
+        int j = i - gap;
+        while (j >= 0 && A[j] > v) {
+            A[j+gap] = A[j];
+            j -= gap;
+            cnt++;
         }
+        A[j+gap] = v;
     }
 }
 
-void selection(Card C[], int N) {
-    for (int i = 0; i < N; i++) {
-        int minv = i;
-        for (int j = i; j < N; j++) {
-            if (C[minv].value > C[j].value) {
-                minv = j;
-            }
+void shellSort(int A[], int N) {
+    for (int h = 1; ;) {
+        if (h > N) {
+            break;
         }
-        Card t = C[i];
-        C[i] = C[minv];
-        C[minv] = t;
+        G.push_back(h);
+        h = h * 3 + 1;
+    }
+
+    for (int i = G.size()-1; i >= 0; i--) {
+        insertionSort(A, N, G[i]);
     }
 }
 
-void print(Card C[], int N) {
-    for (int i = 0; i < N; i++) {
-        if (i > 0) {
-            cout << " ";
-        }
-        cout << C[i].suit << C[i].value;
-    }
-    cout << endl;
-}
-
-bool isSuitable(Card C1[], Card C2[], int N) {
-    for (int i = 0; i < N; i++) {
-        if (C1[i].suit != C2[i].suit) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(){
-    Card C1[100], C2[100];
-    int N;
+    int A[10000], N;
     cin >> N;
     for (int i = 0; i < N; i++) {
-        cin >> C1[i].suit >> C1[i].value;
+        cin >> A[i];
     }
-    for (int i = 0; i < N; i++) {
-        C2[i] = C1[i];
-    }
-
-    bubble(C1, N);
-    print(C1, N);
-    cout << "suitable" << endl;
-    selection(C2, N);
-    print(C2, N);
-    if (isSuitable(C1, C2, N)) {
-        cout << "suitable";
-    } else {
-        cout << "not suitable";
-    }
+    cnt = 0;
+    shellSort(A, N)
 }
